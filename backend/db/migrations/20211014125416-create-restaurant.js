a7a91c64bf913f6d3096e4ebdf6921e391a60e6c
Tonomatic <antonysoly2217@gmail.com>
2021-10-14 09:07:53 -0400

    restaurant model

---
+++ backend/db/migrations/20211014125416-create-restaurant.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Restaurants', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      phoneNumber: {
+        type: Sequelize.INTEGER
+      },
+      hoursOfOperation: {
+        type: Sequelize.STRING
+      },
+      pricing: {
+        type: Sequelize.STRING
+      },
+      cuisine: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      diningOption: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Restaurants');
+  }
+};
