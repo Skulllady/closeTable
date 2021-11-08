2bc6f113d7b912d49b540f82c326b6ef261540a5
Tonomatic <antonysoly2217@gmail.com>
2021-11-08 10:15:25 -0500

    created map

--- frontend/src/components/Restaurant/index.js
+++ frontend/src/components/Restaurant/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import restaurantReducer, { getRestaurant } from '../../store/restaurant';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 function Restaurant() {
     const dispatch = useDispatch();
@@ -13,6 +14,16 @@ function Restaurant() {
         dispatch(getRestaurant(id))
     }, [dispatch, id])
 
+    const containerStyle = {
+        width: '600px',
+        height: '600px'
+    };
+
+    const center = {
+        lat: -3.745,
+        lng: -38.523
+    };
+
     console.log(restaurant)
     console.log(reviews)
     return (
@@ -20,7 +31,7 @@ function Restaurant() {
             <div>
                 We are at this restaurant
                 <div>
-                {restaurant?.id}: {restaurant?.name}
+                    {restaurant?.id}: {restaurant?.name}
 
                 </div>
                 These are the reviews for such restaurant:
@@ -30,6 +41,18 @@ function Restaurant() {
                     </div>
                 ))}
             </div>
+            <LoadScript
+                googleMapsApiKey="AIzaSyABdRIkqcDyFxSoksmn6XCkrU6Z5N2iZao"
+            >
+                <GoogleMap
+                    mapContainerStyle={containerStyle}
+                    center={center}
+                    zoom={10}
+                >
+                    { /* Child components, such as markers, info windows, etc. */}
+                    <></>
+                </GoogleMap>
+            </LoadScript>
         </div>
     );
 }
